c71b4464d79fcd2384f764909a0ffa40cad47e0e
Tushar Ahuja <tusharahuja668@gmail.com>
2025-05-12 23:00:00 +0530

    change in repo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const TodoList = ({todos,handleclick}) => {
+const TodoList = ({ todos, handleclick }) => {
   return (
-    <div><ul>
-        {todos.map((todo)=>(<li key={todo.id}> {todo.text}
-            {todo.completed ?(<span></span>):(<button onClick={()=>handleclick(todo.id)}> Complete</button>)}
-        </li>))}
-        </ul></div>
-  )
-}
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text} -{' '}
+            {todo.completed ? (
+              <span data-testid={`todo-completed-${todo.id}`}>Completed</span>
+            ) : (
+              <button
+                data-testid={`todo-button-${todo.id}`}
+                onClick={() => handleclick(todo.id)}
+              >
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
